ef1a23d01a5fd24cbd4ef22577ebffcecf96706d
Josh Lloyd <j.nevercast@gmail.com>
2014-02-08 18:30:37 +1300

    TEs

---
+++ main/java/net/nevercast/realpower/tileentities/TileEntitySupply.java
@@ -0,0 +1,9 @@
+package net.nevercast.realpower.tileentities;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Created by Josh on 8/02/14.
+ */
+public class TileEntitySupply extends TileEntity {
+}
